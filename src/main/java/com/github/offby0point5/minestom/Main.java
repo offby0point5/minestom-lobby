750dddfb12b8d2c09650e42be2696d6c522e6b01
offbyone <offbyone@disroot.org>
2021-06-18 14:19:30 +0200

    Start server using bootstrap

---
+++ src/main/java/com/github/offby0point5/minestom/Main.java
@@ -0,0 +1,9 @@
+package com.github.offby0point5.minestom;
+
+import net.minestom.server.Bootstrap;
+
+public class Main {
+    public static void main(String[] args) {
+        Bootstrap.bootstrap("com.github.offby0point5.minestom.PreServerStart", args);
+    }
+}
